752ca99bd01760a5222b4deace38694340ba57fc
Ryan Dimitri Ramos <dimitriramos99@gmail.com>
2020-11-19 13:26:52 -0800

    added sout line

--- src/com/dimitriusramos/servantdesignpattern/BlueToothServant.java
+++ src/com/dimitriusramos/servantdesignpattern/BlueToothServant.java
@@ -2,9 +2,9 @@ package com.dimitriusramos.servantdesignpattern;
 
 public class BlueToothServant{
 
-    public void turnOnBlueTooth(MobileDevice device)
-    {
+    public void turnOnBlueTooth(MobileDevice device) {
         if(device.performStatusCheck()){
+            System.out.println("Passed status check.");
             System.out.println("Turning on bluetooth");
         }else{
             System.out.println("Device failed status check cannot turn on bluetooth");
